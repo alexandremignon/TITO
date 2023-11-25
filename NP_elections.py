909f6c4bc8ae59e9691fe3ebf3866de61ce6f3c7
Alexandre Mignon <alexandre.mignon@ilumens.org>
2023-11-25 15:52:03 +0100

    Update NP_elections.py

--- NP_elections.py
+++ NP_elections.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('politics.csv')
+df = pd.read_csv('NP_politics.csv')
 #you can also read your data like this:
 # df = pd.read_csv('/home/charmingdata1/demo-app3/politics.csv')
 
